1bc13148cd36f6f4872edf7a3771ac35359fbc0e
Surajmohite16 <86868898+Surajmohite16@users.noreply.github.com>
2021-07-19 04:48:42 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+
+let countdown = () => {
+    let futureDate = new Date("1 Jan 2022");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+    //console.log(myDate);
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
+    
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+
+}
+
+countdown()
+
+setInterval(countdown, 1000)
